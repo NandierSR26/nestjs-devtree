b860b4443fe0a378924e81e7a748d62a1a2a51ed
Nandier Ruiz <nandier.ruiz@gmail.com>
2025-01-11 13:50:59 -0500

    separando creacion de servidor en un archivo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-import express from 'express'
-
-const app = express()
-
-// Routing
-app.get('/', (req, res) => {
-  res.send('Hola mundo en express / typescript')
-})
+import server from './server'
 
 const port = process.env.PORT || 4000
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Servidor funcionando en el puerto: ${port}`);
 })
